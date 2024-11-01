486c7f115440a975d0f61b7bef01f32e4fcf6b63
Krouta1 <116441828+Krouta1@users.noreply.github.com>
2024-11-01 17:18:45 +0100

    setting up clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+"use client";
 import ModeToggle from "@/components/ModeToggle";
-import Image from "next/image";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Authenticated, Unauthenticated } from "convex/react";
 
 export default function Home() {
   return (
     <main>
       <ModeToggle />
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+      <Authenticated>
+        <UserButton />
+      </Authenticated>
     </main>
   );
 }
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,23 +1,29 @@
+"use client";
 /*This page serves as one definer for all providers */
 
-import { ReactNode } from 'react';
-import { ConvexClientProvider } from '@/app/providers/ConvexClientProvider';
-import { ThemeProvider } from '@/app/providers/ThemeProvider';
+import { ReactNode } from "react";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { convex } from "@/components/providers/ConvexClientProvider";
 
 export function SiteProviders({ children }: { children: ReactNode }) {
   return (
     <>
-      <ConvexClientProvider>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange
-        >
-          {' '}
-          {children}{' '}
-        </ThemeProvider>
-      </ConvexClientProvider>
+      <ClerkProvider
+        publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}
+      >
+        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexProviderWithClerk>
+      </ClerkProvider>
     </>
   );
 }
